4814648c76d4366ea2daaa6955c8ecb267cd7b32
thamlang112 <langtham2k4@gmail.com>
2025-06-14 22:32:16 +0700

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">Hello from Next.js with App Router!</h1>
+    </main>
+  );
+}
